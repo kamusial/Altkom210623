ed987a710260db29bc0fbe1ff2499391221a45a3
kamusial <kamil.support@gmail.com>
2023-06-23 12:57:51 +0200

    Add files via upload

---
+++ selenium7.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://www.w3schools.com/")
+
+# accept = driver.find_element('id', 'accept-choices')
+# accept.click()
+
+driver.maximize_window()
+driver.find_element('id', 'accept-choices').click()
+time.sleep(1)
+
+# driver.find_element('id', 'navbtn_tutorials').click()
+# driver.find_element('xpath', '//*[@id="nav_tutorials"]/div/div/div[2]/a[1]').click()
+
+menu = driver.find_element('id', 'navbtn_tutorials')
+HTMLtutorial = driver.find_element('xpath', '//*[@id="nav_tutorials"]/div/div/div[2]/a[1]')
+
+webdriver.ActionChains(driver).move_to_element(menu).click().move_to_element(HTMLtutorial).click().perform()
+
+HTML_form_attributes = driver.find_element('xpath','//*[@id="leftmenuinnerinner"]/a[40]')
+HTML_form_attributes.click()
+
+tryityourself = driver.find_element('xpath', '//*[@id="main"]/div[3]/a')
+tryityourself.click()
+print(driver.title)
+
+currentWindowName = driver.current_window_handle
+windowNames = driver.window_handles
+#print(windowNames)
+
+for okno in windowNames:
+    if okno != currentWindowName:
+        driver.switch_to.window(okno)
+
+print(driver.title)
+
+driver.switch_to.frame(driver.find_element(By.ID, 'iframeResult'))
+firstName = driver.find_element(By.ID, 'fname')
+
+if firstName.is_enabled():
+    firstName.clear()
+    firstName.send_keys('Kamil')
+else:
+    print('nie da sie kliknac')
+
+driver.close()
+driver.switch_to.window(currentWindowName)
+print(driver.title)
+
+#driver.back()
+
+inputTypes = driver.find_element('xpath', '//*[@id="leftmenuinnerinner"]/a[42]')
+inputTypes.click()
+
+time.sleep(2)
+
+driver.quit()
